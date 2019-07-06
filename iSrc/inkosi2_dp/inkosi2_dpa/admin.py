587f80a57155201f08bcf892bfe3b797a7010574
Technoladie <38797497+t2the2ndpower@users.noreply.github.com>
2019-07-06 17:56:14 -0400

    tried to fix linting

--- iSrc/inkosi2_dp/inkosi2_dpa/admin.py
+++ iSrc/inkosi2_dp/inkosi2_dpa/admin.py
@@ -13,20 +13,22 @@ from .models import IUser, Course, Course_Assignment, Student_Course_Assignment,
 class InkosiUserAdmin(admin.ModelAdmin):
     list_display = ['user_name', 'user_cell', 'user_email']
 
+
 class InkosiCourseAdmin(admin.ModelAdmin):
     list_display = ['course_title', 'course_created_date', 'course_ownerID']
 
+
 class InkosiAssignmentAdmin(admin.ModelAdmin):
     list_display = ['assignment_name', 'assignment_created_date', 'assignment_due_date', 'related_courseID']
-        #list_display_links = ['related_courseID']
-
 
 
 class InkosiTypeAdmin(admin.ModelAdmin):
     list_display = ['student_activity_type', 'student_activity_typeID']
 
+
 class InkosiStuActAdmin(admin.ModelAdmin):
     list_display = ['activity_type', 'activity_details', 'created_date']
+
 
 class InkosiStuCourseAsgnAdm(admin.ModelAdmin):
     list_display = ['student_course_assignmentID', 'isStarted', 'start_date', 'complete_date', 'stu_course_asgn_created_date']
